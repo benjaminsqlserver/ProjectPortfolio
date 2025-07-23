70959ccd01733b4a2ae73b42e59bc1e1b65b6608
Benjamin Fadina <benjaminsqlserver@gmail.com>
2025-07-23 06:39:36 +0100

    End of Time Entry

---
+++ ProjectPortfolio.Domain/Events/TimeEntryApprovedEvent.cs
@@ -0,0 +1,66 @@
+﻿using ProjectPortfolio.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectPortfolio.Domain.Events
+{
+    /// <summary>
+    /// Domain event raised when a time entry is approved.
+    /// This event can be used to notify other parts of the system (e.g., audit log, billing process, reporting)
+    /// that the time entry has transitioned to an approved state.
+    /// </summary>
+    public class TimeEntryApprovedEvent : BaseDomainEvent
+    {
+        /// <summary>
+        /// Gets the ID of the approved time entry.
+        /// </summary>
+        public Guid TimeEntryId { get; }
+
+        /// <summary>
+        /// Gets the ID of the work item associated with the time entry.
+        /// </summary>
+        public Guid WorkItemId { get; }
+
+        /// <summary>
+        /// Gets the ID of the team member who originally logged the time.
+        /// </summary>
+        public Guid TeamMemberId { get; }
+
+        /// <summary>
+        /// Gets the number of hours that were approved.
+        /// </summary>
+        public decimal HoursLogged { get; }
+
+        /// <summary>
+        /// Gets the ID of the user who approved the time entry.
+        /// </summary>
+        public Guid ApprovedBy { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TimeEntryApprovedEvent"/> class with
+        /// the relevant approval details.
+        /// </summary>
+        /// <param name="timeEntryId">The ID of the time entry being approved.</param>
+        /// <param name="workItemId">The ID of the associated work item.</param>
+        /// <param name="teamMemberId">The ID of the team member who logged the time.</param>
+        /// <param name="hoursLogged">The number of hours logged and now approved.</param>
+        /// <param name="approvedBy">The ID of the user who approved the time entry.</param>
+        public TimeEntryApprovedEvent(
+            Guid timeEntryId,
+            Guid workItemId,
+            Guid teamMemberId,
+            decimal hoursLogged,
+            Guid approvedBy)
+        {
+            TimeEntryId = timeEntryId;
+            WorkItemId = workItemId;
+            TeamMemberId = teamMemberId;
+            HoursLogged = hoursLogged;
+            ApprovedBy = approvedBy;
+        }
+    }
+
+}
---
+++ ProjectPortfolio.Domain/Events/TimeEntryUpdatedEvent.cs
@@ -0,0 +1,73 @@
+﻿using ProjectPortfolio.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectPortfolio.Domain.Events
+{
+    /// <summary>
+    /// Domain event raised when a time entry's logged hours are modified.
+    /// This event provides the old and new hour values to support auditing, notification,
+    /// and downstream recalculations (e.g., budget, resource allocation).
+    /// </summary>
+    public class TimeEntryUpdatedEvent : BaseDomainEvent
+    {
+        /// <summary>
+        /// Gets the unique identifier of the updated time entry.
+        /// </summary>
+        public Guid TimeEntryId { get; }
+
+        /// <summary>
+        /// Gets the identifier of the work item associated with the time entry.
+        /// </summary>
+        public Guid WorkItemId { get; }
+
+        /// <summary>
+        /// Gets the identifier of the team member who owns the time entry.
+        /// </summary>
+        public Guid TeamMemberId { get; }
+
+        /// <summary>
+        /// Gets the value of the hours that were originally logged before the update.
+        /// </summary>
+        public decimal PreviousHours { get; }
+
+        /// <summary>
+        /// Gets the new value of the logged hours after the update.
+        /// </summary>
+        public decimal NewHours { get; }
+
+        /// <summary>
+        /// Gets the date that the time was originally logged for.
+        /// </summary>
+        public DateTime LoggedDate { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TimeEntryUpdatedEvent"/> class with all relevant update information.
+        /// </summary>
+        /// <param name="timeEntryId">The ID of the time entry that was updated.</param>
+        /// <param name="workItemId">The ID of the associated work item.</param>
+        /// <param name="teamMemberId">The ID of the team member who logged the time.</param>
+        /// <param name="previousHours">The original hours before the update.</param>
+        /// <param name="newHours">The new hours after the update.</param>
+        /// <param name="loggedDate">The date for which time was logged.</param>
+        public TimeEntryUpdatedEvent(
+            Guid timeEntryId,
+            Guid workItemId,
+            Guid teamMemberId,
+            decimal previousHours,
+            decimal newHours,
+            DateTime loggedDate)
+        {
+            TimeEntryId = timeEntryId;
+            WorkItemId = workItemId;
+            TeamMemberId = teamMemberId;
+            PreviousHours = previousHours;
+            NewHours = newHours;
+            LoggedDate = loggedDate;
+        }
+    }
+
+}
